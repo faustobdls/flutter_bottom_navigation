a4b997b8ea2977180957b63d68ae8aaca6589b07
Fausto Blanco <fausto.level@gmail.com>
2019-04-07 16:40:56 -0300

    upgrade code, using a external package

--- lib/bottom_exemple.dart
+++ lib/bottom_exemple.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:bottom_navigation/custom_docked_fab_locations.dart';
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 
 class BottomExemple extends StatefulWidget {
   @override
@@ -25,97 +26,35 @@ class _BottomExempleState extends State<BottomExemple> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        bottomNavigationBar: BottomAppBar(
-          color: this.bottomAppBarColor, // this.colors['secondary'],
-          clipBehavior: Clip.hardEdge,
-          shape: CircularNotchedRectangle(),
-          notchMargin: 12,
-          child: Padding(
-            padding: EdgeInsets.only(bottom: 30, top: 10),
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                _customButtomBottomBar(
-                    key: Key("vencidas"), icon: Icons.assignment, page: 0, text: "Vencidas"),
-                _customButtomBottomBar(
-                    key: Key("geradas"), icon: Icons.assignment, page: 1, text: "Geradas"),
-                _customButtomBottomBar(
-                    key: Key("pagas"), icon: Icons.assignment, page: 2, text: "Pagas"),
-              ],
-            ),
-          ),
+        backgroundColor: Colors.white,
+        bottomNavigationBar: CurvedNavigationBar(
+          
+          initialIndex: 0,
+          color: this.bottomAppBarColor,
+          backgroundColor: Colors.white,
+          items: <Widget>[
+            Icon(Icons.assignment, size: 48, color: Colors.white,),
+            Icon(Icons.assignment, size: 48, color: Colors.white,),
+            Icon(Icons.assignment, size: 48, color: Colors.white,)
+          ],
+          onTap: (index) {
+            this._pageController.animateToPage(index, curve: Curves.easeInOut, duration: Duration(seconds: 1));
+          },
         ),
-        floatingActionButtonAnimator: FloatingActionButtonAnimator.scaling,
-        floatingActionButton: Container(
-          width: 80,
-          height: 80,
-          child: FloatingActionButton(
-            child: Icon(Icons.assignment, color: Colors.white, size: 48),
-            elevation: 6.0,
-            backgroundColor: this.bottomAppBarColor,
-          ),
-        ),
-        floatingActionButtonLocation: floatingActionButtonLocation,
         body: _pageView());
-  }
-
-  Widget _customButtomBottomBar({Key key, IconData icon, int page, String text}) {
-    return InkWell(
-      onTap: () {
-        // print(details.globalPosition);
-        this._pageController.animateToPage(page,
-          curve: Curves.easeInOut, duration: Duration(seconds: 1));
-      },
-      child: Column(
-        key: key,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisSize: MainAxisSize.min,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Padding(
-            padding: EdgeInsets.only(bottom: 20),
-            child: Icon(
-              icon,
-              size: 36,
-              color: Colors.white,
-            ),
-          ),
-          Text(
-            text,
-            style: TextStyle(color: Colors.white),
-          )
-        ]
-      ),
-    );
   }
 
   void stateOfFABS(int page) {
     setState(() {
       switch (page) {
         case 0:
-          this.floatingActionButtonLocation =
-              CustomDockedFABLocation.rightDocked;
           this.bottomAppBarColor = this.colors['vencidas'];
-          /* _redPosition = _selectPosition;
-          _yellowPosition = _blockPosition;
-          _greenPosition = _blockPosition; */
           break;
         case 1:
-          this.floatingActionButtonLocation =
-              CustomDockedFABLocation.centerDocked;
           this.bottomAppBarColor = this.colors['geradas'];
-          /* _redPosition = _blockPosition;
-          _yellowPosition = _selectPosition;
-          _greenPosition = _blockPosition; */
           break;
         case 2:
-          this.floatingActionButtonLocation =
-              CustomDockedFABLocation.leftDocked;
           this.bottomAppBarColor = this.colors['pagas'];
-          /* _redPosition = _blockPosition;
-          _yellowPosition = _blockPosition;
-          _greenPosition = _selectPosition; */
           break;
         default:
       }
--- lib/main.dart
+++ lib/main.dart
@@ -16,6 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.green,
       ),
+      color: Colors.white,
       home: BottomExemple()
     );
   }
